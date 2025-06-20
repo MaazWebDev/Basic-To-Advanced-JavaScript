a2894557d581f6243a36eee1a3c2745325b4eae9
MaazWebDev <maazkhan66376@gmail.com>
2025-06-20 16:26:33 +0500

    Operator

---
+++ ArthimeticOperator/app.js
@@ -0,0 +1,46 @@
+ var num1 = 50;
+ var num2 = 100;
+
+ var addition = num1 + num2 //Output : 150
+//  alert(addition) 
+console.log(addition); //Show result output in console
+
+ var substraction = num1 - num2
+ console.log(substraction); //Output : -50
+
+ var multiplication = num1 * num2
+ console.log(multiplication); // Output : 5000
+
+ var division = num1 / num2
+ console.log(division); // Output : 0.5
+
+ var modulous = num1 % num2 //Check Even Or Odd
+ console.log(modulous); //Output = 50
+ 
+//BODMAS Rule
+
+var bodmas = multiplication / substraction + addition;
+console.log(bodmas); //Output : 50
+
+//Unfimilar Operator
+
+//Pre Increament
+// ++num , --num Value First Assign Then Update
+
+//Post Increament
+// num++ , num-- Value First Update Then Assign
+
+var number = 50 ;
+var numberAfter = number++
+// var numberAfter = number--
+
+console.log(number); // Output : 51
+console.log(numberAfter); // Output : 50
+
+var number1 = 10 ;
+var numberAfter1 = ++number1 ;
+//var numberAfter1 = --number1 ;
+
+console.log(numb);
+
+
