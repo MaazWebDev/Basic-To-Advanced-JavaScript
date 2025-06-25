f7d868a1fc53388e22d586813be22d9d6e7c92e6
MaazWebDev <maazkhan66376@gmail.com>
2025-06-25 16:08:37 +0500

    ArrayIn

---
+++ Array/app.js
@@ -0,0 +1,34 @@
+var name1 = "Maaz";
+var name2 = "Ahmed";
+var name3 = "Hasan";
+
+//What is Array ? Array store multiple data in it
+
+var names = ["Maaz","Hasan","Ahmed","Sheikh","Shams"];
+console.log(names);
+
+// Length of Array :
+var Length = names.length
+console.log(length);
+
+//Getting Value From Array Index :
+var index = names[2]
+console.log("Index 2 : " + length);
+
+//Last Index of array
+console.log("Last Index : " + names[names.length - 1]);
+
+//First Index of array
+console.log("First Index : " + names[0]);
+
+//Second Last Index of array
+console.log("Second Last Index : " + names[names.length - 2]);
+
+//Update Value In Array
+names[3] = "Sheikh3";
+console.log(names);
+
+//Add Value In Array
+names[names.length] = "Added";
+console.log(names);
+
